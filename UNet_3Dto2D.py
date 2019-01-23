8a0cad11120b11c3c74c682275d375f7dc23119c
iban <itoxir@gmail.com>
2019-01-23 14:10:08 +0100

    create Unet_3Dto2D.py
    first imports to start the module for predicting future raincloud images. 
    to be continued

---
+++ UNet_3Dto2D.py
@@ -0,0 +1,25 @@
+import skimage.io                                     #Used for imshow function
+import skimage.transform                              #Used for resize function
+from skimage.morphology import label                  #Used for Run-Length-Encoding RLE to create final submission
+import scipy.io
+from itertools import chain
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+from sklearn.model_selection import train_test_split
+import sklearn
+import sklearn.model_selection
+
+import tensorflow as tf
+
+from keras.models import Model, load_model
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
+from keras.layers.merge import concatenate, add
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+from scipy.ndimage.filters import gaussian_filter
