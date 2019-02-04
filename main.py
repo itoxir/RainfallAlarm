e8a9b43e717a30ed96218e9b664be2df443a8547
iban <itoxir@gmail.com>
2019-02-04 12:32:11 +0100

    create main,py
    main calls loadtrainingdata.py to scrape website in search for rainfall cloud images.

---
+++ main.py
@@ -0,0 +1,17 @@
+import externals.py
+import LoadRealTimeData.py
+import LoadTrainingData.py
+import UNet_3Dto2D.py
+import geo_plot_rainfall.py
+
+
+load_N_images(1000)
+print(N + ' consecutive images loaded - ready to prepare data for training')
+
+# define and call function to prepare data for sets of 10+1 images to be trained.
+
+# train network 
+
+# predict future frames. 
+
+# cast geolocation of user and plot histogram of rainfall. 
